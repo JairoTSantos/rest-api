6c13ded882a767dae8c33e5837e2a86c7ded397b
Jairo Santos <107869809+JairoTSantos@users.noreply.github.com>
2024-03-20 09:35:33 -0300

    v1

--- controllers/pessoasController.js
+++ controllers/pessoasController.js
@@ -5,7 +5,85 @@ const path = require('path');
 const pessoasModel = require('../models/pessoasModel');
 const usuariosModel = require('../models/usuariosModel');
 
+async function getPeoples(itens, page, orderBy, orderDirection) {
+    try {
+        let order = [];
+        if (orderBy && orderDirection) {
+            order.push([orderBy, orderDirection.toUpperCase()]);
+        }
 
+        const orgaos = await pessoasModel.Pessoas.findAndCountAll({
+            limit: itens,
+            offset: itens * (page - 1),
+            order,
+            include: [
+                {
+                    model: pessoasModel.TiposPessoas,
+                    required: true,
+                    attributes: ['pessoa_tipo_nome', 'pessoa_tipo_descricao']
+                },
+                {
+                    model: usuariosModel.Usuario,
+                    required: true,
+                    attributes: ['usuario_id', 'usuario_nome']
+                }
+            ]
+        });
+
+        const totalPages = Math.ceil(orgaos.count / itens);
+
+        const links = {
+            first: `/api/pessoas?itens=${itens}&pagina=1&ordenarPor=${orderBy}&ordem=${orderDirection}`,
+            self: `/api/pessoas?itens=${itens}&pagina=${page}&ordenarPor=${orderBy}&ordem=${orderDirection}`,
+            last: `/api/pessoas?itens=${itens}&pagina=${totalPages}&ordenarPor=${orderBy}&ordem=${orderDirection}`
+        };
+
+        return { status: 200, message: orgaos.rows.length + ' pessoas encontradas', data: orgaos.rows, links };
+    } catch (error) {
+        return { status: 500, message: 'Erro interno do servidor', error: error };
+    }
+}
+
+async function getPeopleById(id) {
+    try {
+        const orgao = await pessoasModel.Pessoas.findByPk(id ,{
+            include: [
+                {
+                    model: pessoasModel.TiposPessoas,
+                    required: true,
+                    attributes: ['pessoa_tipo_nome', 'pessoa_tipo_descricao']
+                },
+                {
+                    model: usuariosModel.Usuario,
+                    required: true,
+                    attributes: ['usuario_id', 'usuario_nome']
+                }
+            ]
+        });
+
+        if (!orgao) {
+            return { status: 404, message: 'Pessoa não encontrada' };
+        }
+
+        return { status: 200, message: 'Pessoa encontrada', data: orgao };
+    } catch (error) {
+        return { status: 500, message: 'Erro interno do servidor', error: error };
+    }
+}
+
+async function addPeople(data){
+
+    try {
+        await pessoasModel.Pessoas.create(data);
+        return { status: 200, message: 'Pessoa adicionada com sucesso', data: { pessoa_id: data.pessoa_id } };
+    } catch (error) {
+        if (error.original.errno === 1062) {
+            return { status: 409, message: 'Pessoa já inserida' };
+        }
+        return { status: 500, message: 'Erro interno do servidor', error: error };
+    }
+
+}
 
 async function syncPeoples() {
     try {
@@ -26,4 +104,4 @@ async function syncPeoples() {
 }
 
 
-module.exports = { syncPeoples};
+module.exports = { syncPeoples, getPeoples, addPeople, getPeopleById };
--- routes/pessoasRoute.js
+++ routes/pessoasRoute.js
@@ -2,6 +2,33 @@ const express = require('express');
 const router = express.Router();
 const pessoasController = require('../controllers/pessoasController');
 
+router.get('/api/pessoas', async (req, res) => {
+    let itens = parseInt(req.query.itens) || 10;
+    let page = parseInt(req.query.pagina) || 1;
+    let orderBy = req.query.ordenarPor || 'pessoa_nome';
+    let orderDirection = req.query.ordem || 'ASC';
+
+    const resp = await pessoasController.getPeoples(itens, page, orderBy, orderDirection);
+    res.status(resp.status).json(resp);
+});
+
+router.get('/api/pessoas/:id', async (req, res) => {
+    let id = req.params.id;
+
+    const resp = await pessoasController.getPeopleById(id);
+    res.status(resp.status).json(resp);
+});
+
+router.post('/api/pessoas', async (req, res) => {
+    const pessoaPost = { pessoa_nome, pessoa_email, pessoa_telefone, pessoa_endereco, pessoa_bairro, pessoa_estado, pessoa_cep, pessoa_criado_por, pessoa_sexo, pessoa_municipio, pessoa_informacoes, pessoa_tipo_id} = req.body;
+
+    if (!pessoa_nome || !pessoa_email || !pessoa_estado || !pessoa_criado_por || !pessoa_municipio || !pessoa_tipo_id) {
+        return res.status(400).json({ status: 400, message: 'Confira os campos obrigatórios' });
+    }
+    const resp = await pessoasController.addPeople(pessoaPost);
+    res.status(resp.status).json(resp);
+
+});
 
 router.get('/api/pessoas-sync', async (req, res) => {
     const resp = await pessoasController.syncPeoples();

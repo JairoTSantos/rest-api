dc2890910bd0f0ffb485b15adb1501e2d616331e
Jairo Santos <107869809+JairoTSantos@users.noreply.github.com>
2024-03-21 16:08:28 -0300

    v1

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 require('dotenv').config();
 
 app.use(express.json());
 app.use(express.static('public'));
+app.use(cors());
 
 app.use((err, req, res, next) => {
     if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
